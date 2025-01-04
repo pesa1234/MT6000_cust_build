6afbde0f7550dc78442892e82269f8ff30741cf9
Andrea Pesaresi <andreapesaresi82@gmail.com>
2025-01-04 17:04:29 +0100

    upload 2025-01-04_r28760-ec4997cf2f_next-r4.5.5.rss.mtk
    Changelog:
    
    - enabled rss on eth
    - patched ethtool for rss
    - imported mtkhnat_util to set up HQoS (experimental)
    - added HW-ATF if WED enabled
    - new Luci page - Advanced - to set-up: WED - HW-ATF - USB Speed - HQoS
    - rewrited completly the smp_affinity script, now we use the mtk with some improvement
    - update mt76 to 2024.01.04
    - kernel 6.6.69
    
    Happy new year!!!
    
    Signed-off-by: Andrea Pesaresi <andreapesaresi82@gmail.com>

---
+++ 2025-01-04_r28760-ec4997cf2f_next-r4.5.5.rss.mtk/packages/aarch64_cortex-a53/routing/packages.adb
@@ -0,0 +1,4 @@
+ADBdUR_HAŸ»Îþ…ÆÁUÅîvY‰„gVjb/IÌíÌÝNîî\;³·
+g"AôJ=qÐkDA/¢ôPâCõA¤¥õP¢ôÏ¾½Ë¨ß~3ß|¿ï÷›ÙIµ¶èÜ¡ýSaTca4½9M”¡,²ñ,µ¡>QŸÔ“š[|0s²Ïþ0yñY¬6Ra¯|]jj•Ë1—˜`[¸sGy\ñ<Ã6qHÖfŽÂžâW8#\Ü¢§ôn½¾º–¢yâŒê¸M•z8˜É™‹m¦LAKC8žõ\îd1ÈA[Ð<ÔRœsEžS&±6Ã„RP±°Â’%6ei/›¸DbŸY–Ž»MÊ£dN{Ü¢ç™+ŠËçÅ"ƒ•É°'ÁŠ„R¶zw)P&.ÅÒKÂ;Tþç“Jö„cq‡ýk.ˆ9$mÁ‘·âfCòœ!\Åú5r`ôÄév­^Oh@Àm]GqG[w4Ã•qW€'/_øž.nƒÈÏ¡Ìµ¸aâ&é7Ë éÿÍé”Ùn*•“ñ¸ïûºÈ1G³™4uáfã‹§à×	¡ÂFX8–u¥çF…
+È¹*˜"…uârMWÂ;ÙX[öRÚOÂ~› ­ÑÔM¯+‡Š×Ô;íå-ôið£?ß¹À_\^wépÔUÖ#TˆÚÛ€Ðu|nî^ÐêH„«ž>GU!„ªvƒF+ ÐËA\ó§äÌƒØ‹P3ŠT.5Ž¼ö÷ÝˆÝ½p|y6q,\ƒwÝ<ûõÊÕ‘¡'‹bëÏÐÌÁÚê_sçë
+ï;§ü•ù0î1Í‰XÏ¶É½Å7§ë+¢Ï?Ý›_”S³þø$ôÿ
