4737cdceb2687ef2ee48edc5f6951c37ad1eb5cd
Andrea Pesaresi <andreapesaresi82@gmail.com>
2025-04-19 09:45:38 +0200

    upload: 2025-04-19_r29721-5111451803_next-r4.5.34.rss.mtk
    hostapd: update to 2025.04.18
    mt76: update to 2025.04.11
    mac80211: codel param fix
    mtk-eth: backport Mediatek SoC EEE support
    mtk-eth: change QDMA ring size
    mtk-eth: correct max weight of queue on 100mbs
    kernel: update to 6.6.87
    sync with owrt master 2025.04.18
    
    Signed-off-by: Andrea Pesaresi <andreapesaresi82@gmail.com>

---
+++ 2025-04-19_r29721-5111451803_next-r4.5.34.rss.mtk/packages/aarch64_cortex-a53/routing/packages.adb
@@ -0,0 +1,4 @@
+ADBdUR]HTAžÝì,Ì„Š˜èÇ îÝÕÔ‡ÕjýKÄ¤•êEjöÎìÝ¡{çnwæîU±é©ÉÄ^Ä—$’è¥ˆ|¢° ‚µÐ‡ƒò!¨sw3jà»gæÌùæûÎÜ©ojÐ¹ ½/Ã(?ž…ÑÂ®Q†²4ÊÒÄ³ÔöÊXeµÓÜŠÈ¹Ú[íã¼üûÌj|zàÕÜÛâ‰¶ñP¡s‰	¶—a.”ÇÏ1lAL›	…=Å-®úpÚqqƒ^¯wëðíÔµzš#Â`TÇ­*†ÀŒHÎ\l3•qhža8"ÍMÏåÂÄ Çj„æ –â¬ëä8eKÇf˜P
+ÊŽ VŽcÉ<›²”gš—Hì3ËÒqw¤²Ä¸LL†S·¨Ä9æJ âB¿ØIc•aØ“`EB)ÛèAÇIÊÄ¥Xz)Ã±Á•ÿù$‡¼=GX\°-À1AR´\Jˆkdjª.Ž«X¯FªODZºÚµJ¸n öáÖd#îhíŽ¤£2ê:à@˜ÑÂ…KrDÎ{‚2×âF×I?!ƒ¤ÿ7§SvjoF©¬ŒG£¾ïëN–	Íf2£;®-²xÊ~ý·Üai ÑW‘a4¸r®
+¦E`]q=„Šáìl)xÉïWÁ~ë §û/ÔØí™‡Þì«ë¹zß¯ÕÆžŽž]þ˜¾9±¼êv¶!4XhnBÃ.øÜ<¼ —tÌ¢’B%‡A£	èe!núSòæAìA(ŠŠÄïÌùÇGÊ&¯œY_Š5‡¢•ÆÉ#Í'•?j;ž½Ôát¤¿ýçÈÔõñÉO‹{:[Â¸+·67_ã
+µòeàÉbõç¥£ïFWŸO}p¯ôÚëµ@ó7
