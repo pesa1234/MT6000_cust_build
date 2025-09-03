e2db6d1c87fe22c33aa5058aea1f980fe807bff0
Andrea Pesaresi <andreapesaresi82@gmail.com>
2025-09-03 20:57:23 +0200

    upload 2025-09-03_r31571-3ac023616f_next-r4.6.8.rss.mtk
    major changes:
    - mt76 update to 2025.08.25 and update mtk patches
    - mac80211: update to version 6.16 and update mtk patches
    - hostapd: update to Git HEAD (2025-08-26) and update mtk patches
    - kernel: update to 6.12.44
    
    and many more... 599 commit later than latest 4.6.7...
    
    Sorry long summer break :)
    
    Signed-off-by: Andrea Pesaresi <andreapesaresi82@gmail.com>

---
+++ 2025-09-03_r31571-3ac023616f_next-r4.6.8.rss.mtk/packages/aarch64_cortex-a53/routing/packages.adb
@@ -0,0 +1,5 @@
+ADBdURQHAž»Ô
+¥L$‰˜Â"ˆÝ;/ÏÄ8ÐT´‚ dvgnoiwöš™½Sz¹2I¨0¤”"¢¢£!ŠŠˆz«óA‚ê¥$!¢þ½Ë¨oÿ™¾¾ïŸD[«ns:ð*Œ
+c6Œ6D™ÊÑ(KßQëã±xƒ×D]äù|âìôÎÇ†¸Õ¹w_¸§§*?ò­)T¤c[b‚]O0lsåÛÊÎ2ìN,—q…}e;¶Ä)OàV=¡÷éð=¤k	š%ÜdTÇIU8ƒcF¤Ív™J{´Paz<e[¾°¹…AŽÕÍ—âŒð²6eKÏe˜P
+Ê'VžçÈB5e†oYA-‘8ÇGÇ}iÊó$±6|Û¡g™PŠ‹ýb/…Uša_‚	T¶ÚƒŽ{(A±ôÓsÁ•ÿù$‡‚=;6gÿZ€bœ´\Eˆ0Óõý¦'ÐHÃžÈÁî-®Ç4(ÄÉÞ¸3ÙI1FeTxà€[Ñâ…ïêµ]9êsÊ„c›iÜ,s-2HæþætÊö×¤•ÊÈ¦h4—Ëé^†qÍe2­{ÂŠ–8¶a¿þkåËaÁ$ë
+Ï!Œò¥*˜"u	Ä•Z¨€w²PVôRØ¯‡ý 6Cü‘ì/íš¹_ny“:ñø³îØ½õî‹ÈÜÜrùÐÓZàm¬C(_è€ÚF„.ð1˜Ü¼ ÕWÞ‹*CUÖ‚F ÐË@\ó‡2ó ž@¨•T|oºòab8·{¬zêTûÊR¬=¼ý¼$ÆÊ‡4|ýÎÄTMÙæ®Ó¯n|©W~Ù>:>={óØ5 |Z[»øèˆ]zã×âˆ:s8|?Ø:tîöägc|>³¡ß
